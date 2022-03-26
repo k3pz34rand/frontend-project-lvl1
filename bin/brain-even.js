4b32992c7bd54b02012da16cf2a32a36c3e53be7
Alexey Pismerov <pismalx@gmail.com>
2022-03-26 23:09:04 +0300

    fix lint errors brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,40 +1,40 @@
 import readlineSync from 'readline-sync';
-import userName from '../src/cli.js'
+import userName from '../src/cli.js';
 
 const getRandom = () => Math.ceil(Math.random() * 100);
 
 const checkNumber = (number, answer) => {
-    if (number % 2 === 0) {
-        if (answer === 'no'){
-        return 'Correct!';
-        }
-        else return `'${answer}' is wrong answer ;(. Correct answer 'no'`;
+  if (number % 2 === 0) {
+    if (answer === 'no') {
+      return 'Correct!';
     }
-    if (number % 2 !== 0) {
-        if (answer === 'yes') {
-        return 'Correct!';
-        }
-        else return `'${answer}' is wrong answer ;(. Correct answer 'yes'`;
+    return `'${answer}' is wrong answer ;(. Correct answer 'no'`;
+  }
+  if (number % 2 !== 0) {
+    if (answer === 'yes') {
+      return 'Correct!';
     }
-}
+    return `'${answer}' is wrong answer ;(. Correct answer 'yes'`;
+  }
+};
 
 let rightAnswer = 0;
 
 console.log(`Hello, ${userName}`);
-console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
 while (rightAnswer < 3) {
-    const number = getRandom();
-    console.log(`Question: ${number}`)
-    const answer = readlineSync.question(`Your answer: `);
-    const result = checkNumber(number, answer);
-    if (result === 'Correct!') {
-        rightAnswer += 1
-    } else {
-        rightAnswer = 0
-        console.log(result);
-	console.log(`Let's try again, ${userName}`);
-    }
+  const number = getRandom();
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: ');
+  const result = checkNumber(number, answer);
+  if (result === 'Correct!') {
+    rightAnswer += 1;
+  } else {
+    rightAnswer = 0;
+    console.log(result);
+    console.log(`Let's try again, ${userName}`);
+  }
 }
 
 console.log(`Congratulations, ${userName}`);
