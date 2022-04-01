6bbd3047276d87067079745b55b48e2b16158b48
Alexey Pismerov <pismalx@gmail.com>
2022-04-01 21:05:11 +0300

    add brain-calc and src

---
+++ bin/brain-calc.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+import answerCheck, { randomNumb, randomSign, generateQuesion } from '../src/calc.js';
+import userName from '../src/cli.js';
+
+console.log(`Hello, ${userName}!`);
+
+console.log('What is the result of the expression?');
+
+let i = true;
+let counter = 0;
+
+const checkUser = () => {
+  const numb1 = randomNumb();
+  const numb2 = randomNumb();
+  const sign = randomSign();
+
+  console.log(generateQuesion(numb1, numb2, sign));
+
+  const answer = readlineSync.question('Your answer: ');
+  const check = answerCheck(numb1, numb2, sign, answer);
+  console.log(check);
+  if (check !== 'Correct!') {
+    i = false;
+    console.log(`Let's try again, ${userName}!`);
+  }
+  if (counter === 3) {
+    console.log(`Congratulations, ${userName}!`);
+    i = false;
+  }
+  return check;
+};
+
+while (i) {
+  counter += 1;
+  checkUser();
+}
---
+++ src/calc.js
@@ -0,0 +1,37 @@
+export const randomNumb = () => Math.ceil(Math.random() * 100);
+
+export const randomSign = () => Math.ceil(Math.random() * 3);
+
+export const generateQuesion = (numb1, numb2, sign) => {
+  if (sign === 1) {
+    return `Question: ${numb1} + ${numb2}`;
+  }
+  if (sign === 2) {
+    return `Question: ${numb1} - ${numb2}`;
+  }
+  return `Question: ${numb1} * ${numb2}`;
+};
+
+export const checkAnswer = (numb1, numb2, sign, answer) => {
+  if (sign === 1) {
+    if (answer === numb1 + numb2) {
+      return 'Correct!';
+    }
+    return `'${answer}' is wrong answer ;(. Correct answer was '${numb1 + numb2}'.`;
+  }
+  if (sign === 2) {
+    if (answer === numb1 - numb2) {
+      return 'Correct!';
+    }
+    return `'${answer}' is wrong answer ;(. Correct answer was '${numb1 - numb2}'.`;
+  }
+  if (sign === 3) {
+    if (answer === numb1 * numb2) {
+      return 'Correct!';
+    }
+    return `'${answer}' is wrong answer ;(. Correct answer was '${numb1 * numb2}'.`;
+  }
+  return 'error checkAnswer function';
+};
+
+export default checkAnswer;
