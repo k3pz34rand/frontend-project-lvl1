49c1802b571ad51d9b4680efe54d4edf19619024
Alexey Pismerov <pismalx@gmail.com>
2022-04-01 21:37:28 +0300

    fix bug calc

--- src/calc.js
+++ src/calc.js
@@ -14,19 +14,19 @@ export const generateQuesion = (numb1, numb2, sign) => {
 
 export const checkAnswer = (numb1, numb2, sign, answer) => {
   if (sign === 1) {
-    if (answer === numb1 + numb2) {
+    if (answer == numb1 + numb2) {
       return 'Correct!';
     }
     return `'${answer}' is wrong answer ;(. Correct answer was '${numb1 + numb2}'.`;
   }
   if (sign === 2) {
-    if (answer === numb1 - numb2) {
+    if (answer == numb1 - numb2) {
       return 'Correct!';
     }
     return `'${answer}' is wrong answer ;(. Correct answer was '${numb1 - numb2}'.`;
   }
   if (sign === 3) {
-    if (answer === numb1 * numb2) {
+    if (answer == numb1 * numb2) {
       return 'Correct!';
     }
     return `'${answer}' is wrong answer ;(. Correct answer was '${numb1 * numb2}'.`;
